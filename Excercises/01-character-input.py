b1039b95553f69d37ae1abf0620df8b398c82538
ubuntu <patpyhtonkali@gmail.com>
2021-08-05 20:08:14 +0800

    Character Input Solution

--- Excercises/01-character-input.py
+++ Excercises/01-character-input.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+import datetime
+year_now = datetime.datetime.now().year
+
+def character_input():
+    name = str(input("Please enter your name: "))
+    age = int(input("Please enter your age: "))
+    year = (year_now - age) + 100
+
+    print(f"Hello {name} you are {age} years old and in the year {year} you'll be 100 years old")
+
+character_input()
